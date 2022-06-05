ea1498e825d30004ed42512a4f46275f4559a4ce
JesusStan <tipicultbiomassa@gmail.com>
2022-06-05 15:50:23 +0300

    :rocket: Card

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,54 +3,58 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            Item("","")
         }
 
     }
 }
-@Preview (showBackground = true)
 @Composable
-fun NewText(){
-    Row(modifier = Modifier
-        .background(Color.Gray)
-        .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
-
-    ) {
-//        Text(text = "Hello!")
-//        Text(text = "Контент!")
-//        Text(text = "Hello!")
-        Column(
-            modifier = Modifier
-                .background(Color.Red).fillMaxWidth(0.5f)
-                .fillMaxHeight(0.5f),
-            verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.CenterHorizontally
+private fun Item(name: String, description: String){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
         ) {
-                    Text(text = "Hello!")
-                    Text(text = "Контент!")
-                    Text(text = "Hello!")
+            Box(
+             
+                ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(painter = painterResource(id = R.drawable.image), contentDescription = "text", contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                        )
+                    Column() {
+                        Text(text = "Linus tech tips")
+                        Text(text = "Tech youtuber")
+                    }
+                }
+            }
         }
-        Column(modifier = Modifier.background(Color.Blue).fillMaxHeight(0.5f)) {
-            Text(text = "Hello!")
-            Text(text = "Контент!")
-            Text(text = "Hello!")
-        }
-    }
 }
