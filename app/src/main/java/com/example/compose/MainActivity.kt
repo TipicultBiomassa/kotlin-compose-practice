1fd6cadf3683d74a9e1ed8f9b5c0cdf04f138f20
JesusStan <tipicultbiomassa@gmail.com>
2022-06-11 23:16:38 +0300

    :rocket: Click event

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +29,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                Item("", "")
+                Item("", "")
+                Item("", "")
+                Item("", "")
+                Item("", "")
+                Item("", "")
+                Item("", "")
+                Item("", "")
                 Item("", "")
                 Item("", "")
                 Item("", "")
@@ -40,9 +50,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun Item(name: String, description: String){
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(10.dp),
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress { change, dragAmount -> Log.d("Mylog","Long: $dragAmount")  }
+            }
+            .clickable {
+                Log.d("Mylog", "clicked")
+            }
+            .padding(10.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
         ) {
