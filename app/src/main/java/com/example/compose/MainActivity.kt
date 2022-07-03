61d630a04d06f78f72dbe4bcc73e7812a11be91d
JesusStan <tipicultbiomassa@gmail.com>
2022-07-03 22:31:58 +0300

    Cards

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : ComponentActivity() {
                 Item("", "")
                 Item("", "")
                 Item("", "")
+                Item("", "")
             }
         }
 
